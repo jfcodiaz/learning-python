fb400f167e94e2fe1d42a03f6581342b20800ba1
JFco Diaz <jfcodiaz@gmail.com>
2022-04-12 22:40:18 -0500

    if/elif/else

---
+++ 07 - edades/07 - edades.py
@@ -0,0 +1,11 @@
+edad = int(input())
+if edad == 18:
+    print('apenas la libras')
+elif edad > 30:
+     print('ya estas en el tercer nivel')
+elif edad > 25:
+    print('Estas el mera juventud ')
+elif edad > 18:
+    print('eres mayor de edad')
+else: 
+    print('eres menor de edad')
